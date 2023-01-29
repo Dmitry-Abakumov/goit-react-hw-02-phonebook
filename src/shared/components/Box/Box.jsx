f1f834b947c0d4d78e050648b7f48ff4d0925afe
Dmitry-Abakumov <108060418+Dmitry-Abakumov@users.noreply.github.com>
2023-01-29 20:02:47 +0000

    Deploying to gh-pages from @ Dmitry-Abakumov/goit-react-hw-02-phonebook@1c212576f81220246cffe4e059b5c38a604d4114 🚀

--- src/shared/components/Box/Box.jsx
+++
@@ -1,6 +0,0 @@
-import styled from '@emotion/styled';
-import { space, typography, layout, flexbox, grid } from 'styled-system';
-
-const Box = styled('div')(space, typography, layout, flexbox, grid);
-
-export default Box;
